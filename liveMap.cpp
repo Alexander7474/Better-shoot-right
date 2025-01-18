c8c92cf1ed90fe7708e924b4f4364efe470a2eb0
Alexander7474 <alexandre.lanternier@outlook.fr>
2025-01-19 00:50:55 +0100

    Livemap fonctionnel

--- liveMap.cpp
+++ liveMap.cpp
@@ -3,20 +3,36 @@
 #include <GLFW/glfw3.h>
 #include <irrKlang/ik_ESoundOutputDrivers.h>
 #include <irrKlang/irrKlang.h>
+#include <sys/stat.h>
+#include <ctime>
 
 #include "Bbop-Library/include/BBOP/Graphics.h"
 #include "Bbop-Library/include/BBOP/Graphics/cameraClass.h"
+#include "Bbop-Library/include/BBOP/Graphics/mapClass.h"
 
 using namespace std;
 
 Camera *camera;
 
+extern const std::string mapPath = "assets/map/default/";
+extern const std::string mapPathLdtk = "assets/map/default/map.ldtk";
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-  cerr << xoffset << "|" << yoffset << endl;
   camera->setScale(camera->getScale() + (-yoffset)*0.1);
 } 
+
+std::time_t* getFileModificationTime(const std::string& filePath) {
+    struct stat fileStat;
+
+    // Obtenir les informations du fichier
+    if (stat(filePath.c_str(), &fileStat) == 0) {
+      std::time_t* t = new std::time_t(fileStat.st_mtime);  
+      return t;
+    }
+
+  return nullptr;
+}
 
 int main()
 {
@@ -27,12 +43,17 @@ int main()
 
   glfwSwapInterval(1);
 
+  getFileModificationTime(mapPath);
+
   Scene scene;
-  Map map;
   camera = new Camera;
+  Map *map;
+  map = new Map(mapPath.c_str());
 
   Vector2f lastMousePos(0.f,0.f);
   Vector2f lastCamPos = camera->getPosition();
+
+  std::time_t* modifT = getFileModificationTime(mapPathLdtk);
 
   glfwSetScrollCallback(gameWindow, scroll_callback);
 
@@ -41,6 +62,14 @@ int main()
   {
     // clear de la fenêtre en noire
     bbopCleanWindow(gameWindow,Vector3i(0,0,0),1.0f);
+
+    std::time_t* x = getFileModificationTime(mapPathLdtk);
+    if(*modifT != *x){
+      modifT = x;
+      cerr << "Rechargement de la map" << endl;
+      delete map;
+      map = new Map(mapPath.c_str());
+    }
 
     // Mettre a jour la camera 
     if(glfwGetMouseButton(gameWindow, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS){
@@ -69,9 +98,9 @@ int main()
       lastCamPos = camera->getPosition();
     }
 
-    map.update();
+    map->update();
 
-    map.Draw(scene, *camera);
+    map->Draw(scene, *camera);
 
     // vérification des erreurs
     bbopErrorCheck();
