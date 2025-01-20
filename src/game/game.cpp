65da76fc724945a9dc6ffa92fd75ee0bb4752a63
Alexander7474 <alexandre.lanternier@outlook.fr>
2025-01-20 04:09:48 +0100

    Test de lumière directionnel dans la map

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -31,7 +31,7 @@ void Game::update()
   float distance = bbopGetDistance(mainPlayer.getCrossair().getPosition(), mainPlayer.getCharacter().getPosition());
   distance = distance/BBOP_WINDOW_RESOLUTION.x;
 
-  mainPlayerCam.setScale(0.53);
+  mainPlayerCam.setScale(1.0);
   mainPlayerCam.setPosition(middlePos);
 
   mainPlayer.update(&mainPlayerCam, &map);
