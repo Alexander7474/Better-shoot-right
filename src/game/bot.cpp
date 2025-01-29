bcd721be7a07505ab6af201178af04c11cf64b7b
Yassine <yass.elmsebli@gmail.com>
2025-01-29 17:43:15 +0100

    movement bot

--- src/game/bot.cpp
+++ src/game/bot.cpp
@@ -10,38 +10,12 @@
 Bot::Bot(){
     lookAt(Vector2f(getPosition().x-5,getPosition().y));
     sw_look=glfwGetTime();
+    direction=true;
 }
 
 void Bot::Bupdate(Map *map , GameCharacter *user){
     detect_player(user);
-    if (detect)
-    {
-        this->cible=user->getPosition();
-        float espace = cible.x-getPosition().x;
-        if (espace>100.0f)
-        {
-            goRight();
-            lookAt(cible);
-            if (espace<120.0f)
-            {
-                getGun().shoot();
-                getGun().reload();
-            }
-        }else if (espace<-100.0f)
-        {
-            goLeft();
-            lookAt(cible);
-            if (espace>-120.0f)
-            {
-                getGun().shoot();
-                getGun().reload();
-            }                    
-        }else{
-            lookAt(cible);
-            getGun().shoot();
-            getGun().reload();
-        }
-    }else{
+    if(!detect){
         if (sw_look-glfwGetTime()<-1.5)
         {
             if (getLookingPoint().x<getPosition().x)
@@ -56,6 +30,7 @@ void Bot::Bupdate(Map *map , GameCharacter *user){
         }
         
     }
+    
     update(map);  
 }
 
@@ -63,24 +38,83 @@ void Bot::detect_player(GameCharacter * user){
     float espace = user->getPosition().x-getPosition().x;
     if (getLookingPoint().x>getPosition().x)
     {
-        if (espace>-100 && espace<-50)
+        if (espace>-500 && espace<-200)
         {
+            etat=seek;
+        }else if (espace>-200 && espace<-50)
+        {
+            etat=engage;
             detect=true;
             unlock=glfwGetTime();
+            if (espace<-100.0f && espace>-200.0f)
+            {
+                goLeft();
+                lookAt(cible);
+                if (espace>-120.0f)
+                {
+                    getGun().shoot();
+                    getGun().reload();
+                }
+            }else{
+                lookAt(cible);
+                getGun().shoot();
+                getGun().reload();
+            }                
+        }else{
+            etat=patrol;
         }
         
     }else if (getLookingPoint().x<getPosition().x)
     {
-        if (espace<100 && espace>50)
+        if (espace<500 && espace>200)
         {
+            etat=seek;
+        }else if (espace<200 && espace>50)
+        {
+            etat=engage;
             detect=true;
             unlock=glfwGetTime();
+            if (espace>100.0f && espace<200.0f)
+            {
+                goLeft();
+                lookAt(cible);
+                if (espace>-120.0f)
+                {
+                    getGun().shoot();
+                    getGun().reload();
+                }
+            }else{
+                lookAt(cible);
+                getGun().shoot();
+                getGun().reload();
+            }                
+        }else{
+            etat=patrol;
         }
     }
-    if (unlock-glfwGetTime() < -1)
+    if (glfwGetTime()-unlock >3)
     {
         detect=false;
     }
-    
+}
+
+void Bot::patrol_mod(){
+    if (etat=patrol)
+    {
+        if (glfwGetTime()-sw_look<1.5)
+        {
+            if (direction)
+            {
+                goLeft();
+            }else{
+                goRight();
+            }
+            
+        }else{
+            direction=!direction;
+            sw_look=glfwGetTime();
+        }
+        
+    }
     
 }
--- src/game/bot.h
+++ src/game/bot.h
@@ -5,7 +5,11 @@
 #include "../engine/gameCharacter.h"
 #include "../engine/crossair.h"
 
-
+enum State{
+    patrol =1,
+    seek =2,
+    engage =3
+};
 class Bot : public GameCharacter
 {
 private:
@@ -15,12 +19,14 @@ private:
     float sw_look;
     float unlock;
     bool detect;
+    State etat;
 public:
     Bot();
     void Bupdate(Map *map,GameCharacter *perso1);
     void detect_player(GameCharacter *perso1);
+    void patrol_mod();
+    void seek_mod();
     GameCharacter& getCharacter();
-    //Vector2f e_cible();
 };
 
 
